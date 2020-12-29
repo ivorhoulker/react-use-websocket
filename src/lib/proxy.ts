0847dc5f1df00cb22a1ed9f0393552b78e8acad2
ivorhoulker <ivor@rooftopproductions.hk>
2020-12-29 23:26:48 +0800

    trying to use functions

--- src/lib/proxy.ts
+++ src/lib/proxy.ts
@@ -1,34 +1,41 @@
-import { MutableRefObject } from 'react';
+import { MutableRefObject } from "react";
 
-type IfEquals<X, Y, A=X, B=never> =
-  (<T>() => T extends X ? 1 : 2) extends
-  (<T>() => T extends Y ? 1 : 2) ? A : B;
+type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? A
+  : B;
 
 type WritableKeys<T> = {
-  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+  [P in keyof T]-?: IfEquals<
+    { [Q in P]: T[P] },
+    { -readonly [Q in P]: T[P] },
+    P
+  >;
 }[keyof T];
 
 export const websocketWrapper = (
   webSocket: WebSocket,
-  start: MutableRefObject<() => void>,
+  start: MutableRefObject<() => void>
 ): WebSocket => {
-
   return new Proxy<WebSocket>(webSocket, {
     get: (obj, key: keyof WebSocket) => {
       const val = obj[key];
-      if ((key as any) === 'reconnect') return start;
-      if (typeof val === 'function') {
-        console.error('Calling methods directly on the websocket is not supported at this moment. You must use the methods returned by useWebSocket.');
-        
+      if ((key as any) === "reconnect") return start;
+      if (typeof val === "function") {
+        // console.error('Calling methods directly on the websocket is not supported at this moment. You must use the methods returned by useWebSocket.');
+        // Now it's supported, yay.
         //Prevent error thrown by invoking a non-function
-        return () => {};
+        // return () => {};
       } else {
         return val;
       }
     },
-    set: (obj: WebSocket, key:WritableKeys<WebSocket>, val: any) => {
+    set: (obj: WebSocket, key: WritableKeys<WebSocket>, val: any) => {
       if (/^on/.test(key)) {
-        console.warn('The websocket\'s event handlers should be defined through the options object passed into useWebSocket.')
+        console.warn(
+          "The websocket's event handlers should be defined through the options object passed into useWebSocket."
+        );
         return false;
       } else {
         obj[key] = val;
